7578e1f6576d9b6ee10144d116aad6754f9b7026
18582297328 <2622013323@qq.com>
2023-11-16 16:56:29 +0800

    '增加笔记和修改文章'

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -3,7 +3,7 @@ import { getLanguage } from '@/lang/index'
 
 const state = {
   sidebar: {
-    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
+    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : false,
     withoutAnimation: false
   },
   device: 'desktop',
