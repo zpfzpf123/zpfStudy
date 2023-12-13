f3432c26045d3b7f0d04ec69b4ad87f2c8802355
18582297328 <2622013323@qq.com>
2023-12-13 14:42:46 +0800

    ''

--- src/views/study-note/index.vue
+++ src/views/study-note/index.vue
@@ -4,7 +4,7 @@
       <div class="btn">
         <el-form ref="inquire" label-width="80px" :model="inquire" inline>
           <el-form-item label="文章标题" prop="name">
-            <el-input v-model="inquire.name_like" />
+            <el-input v-model="inquire.name_like"/>
           </el-form-item>
           <el-form-item label="文章类型" prop="type">
             <el-select v-model="inquire.type" placeholder="请选择文章类型">
@@ -76,14 +76,14 @@
               <el-button
                 type="primary"
                 @click="viewInfo(scope.$index, scope.row)"
-                >预览
+              >预览
               </el-button>
               <el-button
                 v-if="environment === 'development'"
                 style="margin-left: 20px"
                 type="warning"
                 @click="editInfo(scope.$index, scope.row)"
-                >修改
+              >修改
               </el-button>
               <el-popconfirm
                 style="margin-left: 20px"
@@ -94,7 +94,7 @@
                   v-if="environment === 'development'"
                   slot="reference"
                   type="danger"
-                  >删除文章
+                >删除文章
                 </el-button>
               </el-popconfirm>
             </template>
@@ -111,7 +111,7 @@
       <template v-slot:content>
         <el-form ref="note" :model="note" label-width="120px" :rules="rules">
           <el-form-item label="文章标题" prop="name">
-            <el-input v-model="note.name" />
+            <el-input v-model="note.name"/>
           </el-form-item>
           <el-form-item label="文章类型" prop="type">
             <el-select v-model="note.type" placeholder="请选择文章类型">
@@ -175,16 +175,16 @@
   </div>
 </template>
 <script>
-import Note from "@/api/note";
-import elDialogCom from "@/components/Dialog/el-dialog-com.vue";
-import db from "../../../static/db";
-import github from "@/request/github";
-import { Loading } from "element-ui";
+import Note from '@/api/note'
+import elDialogCom from '@/components/Dialog/el-dialog-com.vue'
+import db from '../../../static/db'
+import github from '@/request/github'
+import { Loading } from 'element-ui'
 
 export default {
-  name: "StudyNote",
+  name: 'StudyNote',
   components: {
-    elDialogCom,
+    elDialogCom
   },
   data() {
     return {
@@ -194,230 +194,231 @@ export default {
       showAddNote: false,
       showDetail: false,
       showToc: false,
-      articleDetails: "",
+      articleDetails: '',
       noteTypeList: [
         {
-          label: "vue",
-          value: "vue",
+          label: 'vue',
+          value: 'vue'
         },
         {
-          label: "js",
-          value: "js",
+          label: 'js',
+          value: 'js'
         },
         {
-          label: "css",
-          value: "css",
+          label: 'css',
+          value: 'css'
         },
         {
-          label: "uniapp",
-          value: "uniapp",
+          label: 'uniapp',
+          value: 'uniapp'
         },
         {
-          label: "ai",
-          value: "ai",
+          label: 'ai',
+          value: 'ai'
         },
         {
-          label: "component",
-          value: "component",
-        },
+          label: 'component',
+          value: 'component'
+        }
       ],
       // 组名信息
       note: {
-        name: "",
-        type: "",
+        name: '',
+        type: '',
         recommendationIndex: 0,
-        content: "",
+        content: ''
       },
       inquire: {
-        name_like: "",
-        type: "",
+        name_like: '',
+        type: ''
       },
       rules: {
-        name: [{ required: true, message: "请输入文章标题", trigger: "blur" }],
+        name: [{ required: true, message: '请输入文章标题', trigger: 'blur' }],
         type: [
-          { required: true, message: "请选择文章类型", trigger: "change" },
-        ],
+          { required: true, message: '请选择文章类型', trigger: 'change' }
+        ]
       },
       column: [
         {
-          prop: "name",
-          label: "标题",
+          prop: 'name',
+          label: '标题'
         },
         {
-          prop: "type",
-          label: "类型",
-        },
-      ],
-    };
+          prop: 'type',
+          label: '类型'
+        }
+      ]
+    }
   },
   computed: {
     condition() {
-      return { name: this.inquire.name_like, type: this.inquire.type };
+      return { name: this.inquire.name_like, type: this.inquire.type }
     },
     environment() {
-      return process.env.NODE_ENV;
-    },
+      return process.env.NODE_ENV
+    }
   },
   watch: {
     condition: {
       handler(val) {
-        this.queryTheArticle();
-      },
-    },
+        this.queryTheArticle()
+      }
+    }
   },
   mounted() {
-    this.init();
+    this.init()
   },
   methods: {
     async handleUploadImage(event, insertImage, files) {
-      const reader = new FileReader();
+      const reader = new FileReader()
 
       function getBase64(file) {
         return new Promise((resolve) => {
-          reader.onload = function (event) {
-            const fileContent = event.target.result;
-            resolve(fileContent.split(",")[1]);
-          };
-          reader.readAsDataURL(file);
-        });
+          reader.onload = function(event) {
+            const fileContent = event.target.result
+            resolve(fileContent.split(',')[1])
+          }
+          reader.readAsDataURL(file)
+        })
       }
 
-      const content = await getBase64(files[0]); // 目前上传接口支持上传base64格式图片，所以先将不是base6格式的文件流转化成base64,如果是base64格式忽略这一步
+      const content = await getBase64(files[0]) // 目前上传接口支持上传base64格式图片，所以先将不是base6格式的文件流转化成base64,如果是base64格式忽略这一步
       const loadingInstance = Loading.service({
         fullscreen: true,
-        text: "图片上传中...",
-      });
-      const imgUrl = await github.uploader(content, files[0]);
+        text: '图片上传中...'
+      })
+      const imgUrl = await github.uploader(content, files[0])
       if (imgUrl) {
         this.$message.success(
-          "上传github成功！！回显由于网络延迟，显示不出来或显示较缓慢属于正常现象"
-        );
+          '上传github成功！！回显由于网络延迟，显示不出来或显示较缓慢属于正常现象'
+        )
         this.$nextTick(() => {
           // 以服务的方式调用的 Loading 需要异步关闭
-          loadingInstance.close();
-        });
+          loadingInstance.close()
+        })
       }
       // 此处只做示例
       insertImage({
         url: `https://github.com/zpfzpf123/images/blob/master/${files[0].name}?raw=true`,
-        desc: "files.name",
-        width: "auto",
-        height: "auto",
-      });
+        desc: 'files.name',
+        width: 'auto',
+        height: 'auto'
+      })
     },
     init() {
-      Note.getNote()
-        .then((list) => {
-          console.log(list);
-          this.noteList = [];
-          this.noteList.push(...list);
-        })
-        .catch((_) => {
-          this.noteList = [];
-          this.noteList.push(...db.note);
-          this.initList = [];
-          this.initList.push(...db.note);
-        });
+      if (this.environment === 'development') {
+        Note.getNote()
+          .then((list) => {
+            console.log(list)
+            this.noteList = []
+            this.noteList.push(...list)
+          })
+      } else {
+        this.noteList = []
+        this.noteList.push(...db.note)
+        this.initList = []
+        this.initList.push(...db.note)
+      }
     },
     addNote() {
-      this.status = 0;
-      this.showAddNote = true;
+      this.status = 0
+      this.showAddNote = true
       this.note = Object.assign(
         {},
         {
-          name: "",
-          type: "",
+          name: '',
+          type: '',
           recommendationIndex: 0,
-          content: "",
-          id: "",
+          content: '',
+          id: ''
         }
-      );
+      )
       this.$nextTick(() => {
-        this.$refs.note.resetFields();
-      });
+        this.$refs.note.resetFields()
+      })
     },
     queryTheArticle() {
       const condition = Object.fromEntries(
         Object.entries(this.inquire).filter(
-          ([key, value]) => value != null && value !== ""
+          ([key, value]) => value != null && value !== ''
         )
-      );
-      Note.getNote(condition)
-        .then((list) => {
-          this.noteList = [];
-          this.noteList.push(...list);
+      )
+      if (this.environment === 'development') {
+        Note.getNote(condition)
+          .then((list) => {
+            this.noteList = []
+            this.noteList.push(...list)
+          })
+      } else {
+        this.noteList = this.initList.filter((item) => {
+          return (
+            item.name.includes(this.inquire.name_like) &&
+            item.type.includes(this.inquire.type)
+          )
         })
-        .catch((_) => {
-          console.log(this.inquire);
-          this.noteList = this.initList.filter((item) => {
-            return (
-              item.name.includes(this.inquire.name_like) &&
-              item.type.includes(this.inquire.type)
-            );
-          });
-        });
+      }
     },
     reset() {
       this.inquire = Object.assign(
         {},
         {
-          name_like: "",
-          type: "",
+          name_like: '',
+          type: ''
         }
-      );
+      )
     },
     closeAddNote() {
-      this.showAddNote = false;
+      this.showAddNote = false
     },
     submitForm() {
-      console.log(this.note);
+      console.log(this.note)
       this.$refs.note.validate((valid) => {
         if (!valid) {
-          return false;
+          return false
         }
         if (!this.note.recommendationIndex) {
-          this.$message.warning("请选择推荐指数");
-          return false;
+          this.$message.warning('请选择推荐指数')
+          return false
         }
         if (!this.note.content) {
-          this.$message.warning("请填写文章内容");
-          return false;
+          this.$message.warning('请填写文章内容')
+          return false
         }
-        console.log(this.note);
+        console.log(this.note)
         if (this.status === 0) {
-          const id = this.noteList.length + 1;
+          const id = this.noteList.length + 1
           Note.postNote({
             ...this.note,
-            id,
+            id
           }).then((_) => {
-            this.$message.success("添加文章成功！");
-            this.init();
-            this.showAddNote = false;
-          });
+            this.$message.success('添加文章成功！')
+            this.init()
+            this.showAddNote = false
+          })
         } else if (this.status === 1) {
           Note.putNote(this.note.id, this.note).then((res) => {
-            this.$message.success("修改文章成功！");
-            this.init();
-            this.showAddNote = false;
-          });
+            this.$message.success('修改文章成功！')
+            this.init()
+            this.showAddNote = false
+          })
         }
-      });
+      })
     },
     closeDetail() {
-      this.showDetail = false;
+      this.showDetail = false
     },
     viewInfo(index, val) {
-      this.showDetail = true;
+      this.showDetail = true
       this.$nextTick(() => {
-        this.articleDetails = val.content;
+        this.articleDetails = val.content
         setTimeout(() => {
-          this.showToc = true;
-        }, 1000);
-      });
+          this.showToc = true
+        }, 1000)
+      })
     },
     editInfo(index, val) {
-      this.status = 1;
-      this.showAddNote = true;
+      this.status = 1
+      this.showAddNote = true
       this.note = Object.assign(
         {},
         {
@@ -425,23 +426,23 @@ export default {
           type: val.type,
           recommendationIndex: val.recommendationIndex,
           content: val.content,
-          id: val.id,
+          id: val.id
         }
-      );
-      console.log(this.note);
-      this.$refs.note.resetFields();
+      )
+      console.log(this.note)
+      this.$refs.note.resetFields()
     },
     delInfo(index, val) {
-      console.log(index, val);
+      console.log(index, val)
       Note.delNote(val.id).then((res) => {
-        this.$message.success("删除成功！");
-      });
+        this.$message.success('删除成功！')
+      })
     },
     handleCopyCodeSuccess() {
-      this.$message.success("复制成功！");
-    },
-  },
-};
+      this.$message.success('复制成功！')
+    }
+  }
+}
 </script>
 <style scoped lang="scss">
 #btn-table {
@@ -471,35 +472,42 @@ export default {
   .table {
     width: 100%;
     flex: 6;
-     ::v-deep .el-table {
-       .el-table__body-wrapper {
+
+    ::v-deep .el-table {
+      .el-table__body-wrapper {
         // 滚动条整体部分
         &::-webkit-scrollbar {
           width: 6px;
           height: 6px;
         }
+
         // 滚动条的轨道的两端按钮，允许通过点击微调小方块的位置。
         &::-webkit-scrollbar-button {
           display: none;
         }
+
         // 滚动条的轨道（里面装有Thumb）
         &::-webkit-scrollbar-track {
           background: transparent;
         }
+
         // 滚动条的轨道（里面装有Thumb）
         &::-webkit-scrollbar-track-piece {
           background-color: transparent;
         }
+
         // 滚动条里面的小方块，能向上向下移动（或往左往右移动，取决于是垂直滚动条还是水平滚动条）
         &::-webkit-scrollbar-thumb {
           background: #42b983;
           cursor: pointer;
           border-radius: 4px;
         }
+
         // 边角，即两个滚动条的交汇处
         &::-webkit-scrollbar-corner {
           display: none;
         }
+
         // 两个滚动条的交汇处上用于通过拖动调整元素大小的小控件
         &::-webkit-resizer {
           display: none;
